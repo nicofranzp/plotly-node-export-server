90db6da4061549a1e66103dcd69ff6c153c0991c
Nicolas Franz-Pattillo <nicofranzp@gmail.com>
2023-11-09 14:48:23 -0300

    cleanup

--- export-server/ssr.js
+++ export-server/ssr.js
@@ -1,57 +1,23 @@
-/*
-    Plotly.js offline image export server with Node.js
-    Copyright (C) 2018, 2021, 2022, 2023  Dirk Stolle
+var fs = require("fs");
+const jsdom = require("jsdom");
+const { v4: uuidv4 } = require("uuid");
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-var fs = require('fs');
-const jsdom = require('jsdom');
-const { v4: uuidv4 } = require('uuid');
-
-/* Renders JSON data for a Plotly.js plot into a SVG file.
-
-   Parameters:
-     jsonData - (string) the JSON data required by Plotly.js
-     filename - (string) desired output file name for the SVG file
-     width    - (number) width of the SVG file in pixels
-     height   - (number) height of the SVG file in pixels
-
-   Returns:
-     object that contains two members:
-       success - (boolean) indicates success of the rendering process
-       filename - (string) filename of the output, only present after successful
-                  rendering
-       failure - (string) reason for render failure; only present after failed
-                 rendering, may be cryptic and is not necessarily human-friendly
-*/
-exports.render = async function(jsonData, filename, width, height) {
+render = async function (jsonData, filename, width, height) {
   const unique_id = uuidv4();
 
   if (!filename) {
-    filename = 'graph-' + unique_id + '.svg';
+    filename = "graph-" + unique_id + ".svg";
   }
-  if (typeof jsonData !== 'string') {
+  if (typeof jsonData !== "string") {
     return {
       success: false,
-      failure: 'json-not-a-string'
+      failure: "json-not-a-string",
     };
   }
-  if (typeof filename !== 'string') {
+  if (typeof filename !== "string") {
     return {
       success: false,
-      failure: 'filename-not-a-string'
+      failure: "filename-not-a-string",
     };
   }
 
@@ -67,9 +33,9 @@ exports.render = async function(jsonData, filename, width, height) {
   } else {
     height = parsed_height;
   }
-  // Handle image size and fallback to 700 x 400, if necessary.
+  // Handle image size and fallback to 700 x 400 if not provided correctly.
   width = width || 700;
-  height = height || 400
+  height = height || 400;
 
   const parsed_data = JSON.parse(jsonData);
   const data = parsed_data.data || parsed_data;
@@ -78,32 +44,47 @@ exports.render = async function(jsonData, filename, width, height) {
 
   const virtual_console = new jsdom.VirtualConsole();
   virtual_console.sendTo(console);
-  const win = new jsdom.JSDOM('', { runScripts: 'dangerously', virtual_console }).window;
-  win.HTMLCanvasElement.prototype.getContext = function() { return null; };
-  win.URL.createObjectURL = function() { return null; };
+  const win = new jsdom.JSDOM("", {
+    runScripts: "dangerously",
+    virtual_console,
+  }).window;
+  win.HTMLCanvasElement.prototype.getContext = function () {
+    return null;
+  };
+  win.URL.createObjectURL = function () {
+    return null;
+  };
 
   let result = {
     success: false,
-    failure: 'promise-not-resolved'
+    failure: "promise-not-resolved",
   };
 
-  await
-  fs.promises.readFile('./plotly-2.26.2.min.js', 'utf-8')
+  await fs.promises
+    .readFile("./plotly-2.26.2.min.js", "utf-8")
     .then(win.eval)
-    .then(() => win.Plotly.toImage({data: [data], layout: layout, config: config},
-                                   { format: 'svg', imageDataOnly: true }))
-    .then(function(data) {
+    .then(() =>
+      win.Plotly.toImage(
+        { data: data, layout: layout, config: config },
+        { format: "svg", imageDataOnly: true }
+      )
+    )
+    .then(function (data) {
       fs.promises.writeFile(filename, data);
     })
     .then(() => {
       result = { success: true, filename: filename };
       return result;
     })
-    .catch(function(error) {
+    .catch(function (error) {
       console.log("Error occurred: " + error);
-      result = { success: false, failure: 'promise-rejected' };
+      result = { success: false, failure: "promise-rejected" };
       return result;
     });
 
   return result;
+};
+
+module.exports = {
+  render,
 };
--- export-server/test.js
+++ export-server/test.js
@@ -1,17 +1,85 @@
-var request = require("request");
-
-request.post(
-  "http://localhost:3000/",
-  {
-    json: {
-      x: ["2013-10-04 22:23:00", "2013-11-04 22:23:00", "2013-12-04 22:23:00"],
-      y: [1, 3, 6],
-      type: "scatter",
+const { render } = require("./ssr");
+// run the server
+var test1 = {
+  data: [
+    {
+      y: [1.19, 1.06, 1.06],
+      x: ["apr.21", "sep.21", "Stressed"],
+      type: "bar",
+      visible: true,
+      name: "Effective",
+      marker: { color: "rgb(112,173,71)" },
     },
+    {
+      y: [null, null, 0.89],
+      x: ["apr.21", "sep.21", "Stressed"],
+      type: "bar",
+      visible: true,
+      name: "Rates shock",
+      marker: { color: "rgb(175,171,171)" },
+    },
+    {
+      y: [null, null, 0.47],
+      x: ["apr.21", "sep.21", "Stressed"],
+      type: "bar",
+      visible: true,
+      name: "Inflationary shock",
+      marker: { color: "rgb(59,56,56)" },
+    },
+    {
+      y: [null, null, 0.43],
+      x: ["apr.21", "sep.21", "Stressed"],
+      type: "bar",
+      visible: true,
+      name: "Unemployment shock",
+      marker: { color: "rgb(84,130,53)" },
+    },
+    {
+      y: [null, null, 0],
+      x: ["apr.21", "sep.21", "Stressed"],
+      type: "bar",
+      visible: true,
+      name: "total",
+      text: [null, null, "2,87"],
+      hovertemplate: "<b>Total</b>:%{text}",
+      hoverlabel: { namelength: 0 },
+      marker: { color: "rgb(112,173,71)" },
+      showlegend: false,
+    },
+  ],
+  layout: {
+    barmode: "stack",
+    font: {
+      family:
+        "-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'",
+      size: 14,
+    },
+    xaxis: {
+      tickcolor: "rgba(33, 37, 41, 0.1)",
+      gridcolor: "rgba(33, 37, 41, 0.1)",
+      automargin: true,
+      tickson: "boundaries",
+      showdividers: true,
+      ticklen: 15,
+    },
+    yaxis: {
+      zerolinecolor: "rgb(33, 37, 41)",
+      tickcolor: "rgba(33, 37, 41, 0.1)",
+      gridcolor: "rgba(33, 37, 41, 0.1)",
+      automargin: true,
+    },
+    legend: {
+      orientation: "h",
+      bgcolor: "rgba(0, 0, 0, 0)",
+      x: 0,
+      y: 1.1,
+    },
+    showlegend: true,
+    plot_bgcolor: "rgb(255, 255, 255)",
+    paper_bgcolor: "rgb(255, 255, 255)",
+    margin: { l: 40, r: 20, b: 60, t: 20 },
   },
-  function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-    }
-  }
-);
+};
+// test1.layout.width = 400;
+// test1.layout.height = 400;
+render(JSON.stringify(test1), "test.svg").then((a) => console.log(a));
