aefeaeb6eb4bc107faa7e4a4149a90cb9e627c04
Nicolas Franz-Pattillo <9399502+nicofranzp@users.noreply.github.com>
2024-04-04 17:24:29 -0300

    random commit

--- plotly2SVG.js
+++ plotly2SVG.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs-extra");
 
-var exp = function (plotlyData, layout) {
+var exp = function (plotlyData, layout, config) {
   return new Promise(async (resolve, reject) => {
     try {
       const browser = await puppeteer.launch({ headless: "new" });
@@ -17,16 +17,15 @@ var exp = function (plotlyData, layout) {
       await page.addScriptTag({
         url: "https://cdnjs.cloudflare.com/ajax/libs/plotly.js/2.27.1/plotly.min.js",
       });
-
-      await page.exposeFunction("plotlyData", plotlyData);
-      await page.exposeFunction("layout", layout);
-
+      // await page.exposeFunction("plotlyData", plotlyData);
+      // await page.exposeFunction("layout", layout);
+      // await page.exposeFunction("config", config);
       let image = await page.evaluate(
-        function (plotlyData, layout) {
+        function (plotlyData, layout, config) {
           // @ts-ignore
           const Plotly = window.Plotly;
           return Plotly.toImage(
-            { data: plotlyData, layout: layout },
+            { data: plotlyData, layout: layout, config: config },
             { format: "svg", imageDataOnly: true }
           );
         },
@@ -44,7 +43,7 @@ var exp = function (plotlyData, layout) {
 
 function export2SVG(data, layout, config, plotname) {
   return new Promise(async (resolve, reject) => {
-    let imageBuffer = await exp(data, layout);
+    let imageBuffer = await exp(data, layout, config);
     await fs.writeFile(plotname + ".svg", imageBuffer);
   });
 }
--- test.js
+++ test.js
@@ -1,87 +1,92 @@
-const { render } = require("./ssr");
-// const { plot } = require("@mhkeller/plot");
-
-// run the server
-var test1 = {
-  data: [
-    {
-      y: [1.19, 1.06, 1.06],
-      x: ["apr.21", "sep.21", "Stressed"],
-      type: "bar",
-      visible: true,
-      name: "Effective",
-      marker: { color: "rgb(112,173,71)" },
-    },
-    {
-      y: [null, null, 0.89],
-      x: ["apr.21", "sep.21", "Stressed"],
-      type: "bar",
-      visible: true,
-      name: "Rates shock",
-      marker: { color: "rgb(175,171,171)" },
-    },
-    {
-      y: [null, null, 0.47],
-      x: ["apr.21", "sep.21", "Stressed"],
-      type: "bar",
-      visible: true,
-      name: "Inflationary shock",
-      marker: { color: "rgb(59,56,56)" },
-    },
-    {
-      y: [null, null, 0.43],
-      x: ["apr.21", "sep.21", "Stressed"],
-      type: "bar",
-      visible: true,
-      name: "Unemployment shock",
-      marker: { color: "rgb(84,130,53)" },
-    },
-    {
-      y: [null, null, 0],
-      x: ["apr.21", "sep.21", "Stressed"],
-      type: "bar",
-      visible: true,
-      name: "total",
-      text: [null, null, "2,87"],
-      hovertemplate: "<b>Total</b>:%{text}",
-      hoverlabel: { namelength: 0 },
-      marker: { color: "rgb(112,173,71)" },
-      showlegend: false,
-    },
-  ],
-  layout: {
-    barmode: "stack",
-    font: {
-      family:
-        "-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'",
-      size: 14,
-    },
-    xaxis: {
-      tickcolor: "rgba(33, 37, 41, 0.1)",
-      gridcolor: "rgba(33, 37, 41, 0.1)",
-      automargin: true,
-      tickson: "boundaries",
-      showdividers: true,
-      ticklen: 15,
-    },
-    yaxis: {
-      zerolinecolor: "rgb(33, 37, 41)",
-      tickcolor: "rgba(33, 37, 41, 0.1)",
-      gridcolor: "rgba(33, 37, 41, 0.1)",
-      automargin: true,
-    },
-    legend: {
-      orientation: "h",
-      bgcolor: "rgba(0, 0, 0, 0)",
-      x: 0,
-      y: 1.1,
-    },
-    showlegend: true,
-    plot_bgcolor: "rgb(255, 255, 255)",
-    paper_bgcolor: "rgb(255, 255, 255)",
-    margin: { l: 40, r: 20, b: 60, t: 20 },
+const { export2SVG } = require("./plotly2SVG");
+var config = {
+  toImageButtonOptions: {
+    format: "png",
+    filename: "grafico_IEF",
+    height: 500,
+    width: 700,
+    scale: 2,
   },
+  modeBarButtonsToRemove: [
+    "zoomIn2d",
+    "zoomOut2d",
+    "autoScale2d",
+    "toggleSpikelines",
+    "select2d",
+    "lasso2d",
+  ],
+  showEditInChartStudio: false,
+  displaylogo: false,
+  responsive: true,
 };
-test1.layout.width = 400;
-test1.layout.height = 400;
-render(JSON.stringify(test1), "test.svg").then((a) => console.log(a));
+var data = [
+  {
+    y: [null, null, 0.89],
+    x: ["apr.21", "sep.21", "Stressed"],
+    type: "bar",
+    visible: true,
+    name: "Rates shock",
+    marker: { color: "rgb(175,171,171)" },
+  },
+  {
+    y: [null, null, 0.47],
+    x: ["apr.21", "sep.21", "Stressed"],
+    type: "bar",
+    visible: true,
+    name: "Inflationary shock",
+    marker: { color: "rgb(59,56,56)" },
+  },
+  {
+    y: [null, null, 0.43],
+    x: ["apr.21", "sep.21", "Stressed"],
+    type: "bar",
+    visible: true,
+    name: "Unemployment shock",
+    marker: { color: "rgb(84,130,53)" },
+  },
+  {
+    y: [null, null, 0],
+    x: ["apr.21", "sep.21", "Stressed"],
+    type: "bar",
+    visible: true,
+    name: "total",
+    text: [null, null, "2,87"],
+    hovertemplate: "<b>Total</b>:%{text}",
+    hoverlabel: { namelength: 0 },
+    marker: { color: "rgb(112,173,71)" },
+    showlegend: false,
+  },
+];
+var layout = {
+  barmode: "stack",
+  font: {
+    family:
+      "-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'",
+    size: 14,
+  },
+  xaxis: {
+    tickcolor: "rgba(33, 37, 41, 0.1)",
+    gridcolor: "rgba(33, 37, 41, 0.1)",
+    automargin: true,
+    tickson: "boundaries",
+    showdividers: true,
+    ticklen: 15,
+  },
+  yaxis: {
+    zerolinecolor: "rgb(33, 37, 41)",
+    tickcolor: "rgba(33, 37, 41, 0.1)",
+    gridcolor: "rgba(33, 37, 41, 0.1)",
+    automargin: true,
+  },
+  legend: {
+    orientation: "h",
+    bgcolor: "rgba(0, 0, 0, 0)",
+    x: 0,
+    y: 1.1,
+  },
+  showlegend: true,
+  plot_bgcolor: "rgb(255, 255, 255)",
+  paper_bgcolor: "rgb(255, 255, 255)",
+  margin: { l: 10, r: 10, b: 20, t: 10 },
+};
+export2SVG(data, layout, config, "gName");
